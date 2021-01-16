fd991fa1e87ab707aa21b23c2f0287da50bab629
Milos R <47392105+milossql@users.noreply.github.com>
2021-01-16 11:52:48 +0100

    Add files via upload

---
+++ MVPs4Croatia/Code/02 Parameter Sniffing _cache.sql
@@ -0,0 +1,5 @@
+SELECT cplan.usecounts, cplan.objtype, qtext.text, qplan.query_plan
+FROM sys.dm_exec_cached_plans AS cplan
+CROSS APPLY sys.dm_exec_sql_text(plan_handle) AS qtext
+CROSS APPLY sys.dm_exec_query_plan(plan_handle) AS qplan
+ORDER BY cplan.usecounts DESC
